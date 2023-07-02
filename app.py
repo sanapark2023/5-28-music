8eb4ef7278facef4ec0e378d617c2666407bc5c0
sanapark2023 <126647810+sanapark2023@users.noreply.github.com>
2023-07-02 14:04:04 +0900

    수정

--- app.py
+++ app.py
@@ -6,13 +6,35 @@ app = Flask(__name__)
 def main():
     return render_template('main.html')
 
-@app.route('/submit',method=['POST'])
+@app.route('/submit',methods=['POST'])
 def submit():
     if request.method == 'POST':
         year = request.form.get('year')
         genre = request.form.get('genre')
         if year and genre:
-            return render_template('list.html')
+            if year == 'a':
+                if genre == 'ballad':
+                    list = ['']
+                elif genre == 'rap':
+                    list = ['']
+                elif genre == 'dance':
+                    list = ['']
+                elif genre == 'pop':
+                    list = ['']
+                elif genre == 'hiphop':
+                    list = ['']
+            elif year == 'b':
+                if genre == 'ballad':
+                    list = ['']
+                elif genre == 'rap':
+                    list = ['']
+                elif genre == 'dance':
+                    list = ['']
+                elif genre == 'pop':
+                    list = ['']
+                elif genre == 'hiphop':
+                    list = ['']
+            return render_template('list.html', list = list)
         else:
             error_message = "연도와 장르 모두 선택해주세요."
             return render_template('main.html',error_message = error_message)
